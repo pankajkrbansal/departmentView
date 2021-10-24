c5c69c16243c98849a16f3cc8b704f3a51ca89bb
pankajkrbansal <pankajbansal.pkb@gmail.com>
2021-10-24 14:56:23 +0530

    minor bug fix.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const cors = require('cors');
-const setup = require('./model/dbSetup');
-// const router = require('./routing/route');
-
+const setupdb = require('./model/dbSetup');
+const router = require('./routing/routing');
+const requestLogger = require('./utilities/requestLogger');
+const errorLogger = require('./utilities/errorLogger')
 const app = express();
 
 app.use(cors());
 
-// app.use(requestLogger);
+app.use(requestLogger);
 app.use('/',router);
-// app.use(errorLogger);
+app.use(errorLogger);
 
 app.use('/setupDb',(req,res,next)=>{
-    setup.setupDb().then((result)=>{
+    setupdb.setupDb().then((result)=>{
         res.send(result)
     }).catch(error=>{
         next(error)
--- backend/src/routing/routing.js
+++ backend/src/routing/routing.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router;
+const router = express.Router();
 const service = require('../service/user')
 
 router.get('/departments',(req,res,next)=>{
